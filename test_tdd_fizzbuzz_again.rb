018b7ed5d98b007c94c7c19794f3ebd105c28c08
William Mullins <twfdisturbed@hotmail.com>
2018-06-23 15:06:10 -0400

    added test_assert_that_result_is__an_array()

--- test_tdd_fizzbuzz_again.rb
+++ test_tdd_fizzbuzz_again.rb
@@ -6,5 +6,7 @@ class TestYourFunctions < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-
+    def test_assert_that_result_is__an_array()
+    	assert_equal(Array, fizzbuzz().class)
+    end
 end
