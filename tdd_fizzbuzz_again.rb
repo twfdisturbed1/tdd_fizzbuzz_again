6003d9f1aa450305f8fe6f32a202b9680245c624
William Mullins <twfdisturbed@hotmail.com>
2018-06-26 05:17:04 -0400

    Added code to change every 15th line to mined minds

--- tdd_fizzbuzz_again.rb
+++ tdd_fizzbuzz_again.rb
@@ -1,3 +1,14 @@
 def fizzbuzz()
     fizzbuzzarry = [*1..100]
+    new_arr = []
+    fizzbuzzarry.each do |item|
+		if item %3 == 0 && item %5 == 0
+            item = "mined minds"
+        else
+        end
+        new_arr << item
+    end
+  
+	new_arr
 end
+
--- test_tdd_fizzbuzz_again.rb
+++ test_tdd_fizzbuzz_again.rb
@@ -12,7 +12,7 @@ class TestYourFunctions < Minitest::Test
     def test_assert_array_length_is_100()
         assert_equal(100, fizzbuzz().length)
     end
-        def test_assert_first_position_is_1()
+    def test_assert_first_position_is_1()
     	assert_equal(1, fizzbuzz().first)
     end
     def test_assert_fifteenth_position_is_mined_minds()
