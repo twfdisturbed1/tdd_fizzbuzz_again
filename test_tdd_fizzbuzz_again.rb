de3894ef54dedb5106e0dfb92f980a5cbb0f02fd
William Mullins <twfdisturbed@hotmail.com>
2018-06-26 04:39:49 -0400

    Added line to test for every 15th line to be mined minds

--- test_tdd_fizzbuzz_again.rb
+++ test_tdd_fizzbuzz_again.rb
@@ -9,4 +9,13 @@ class TestYourFunctions < Minitest::Test
     def test_assert_that_result_is__an_array()
     	assert_equal(Array, fizzbuzz().class)
     end
+    def test_assert_array_length_is_100()
+        assert_equal(100, fizzbuzz().length)
+    end
+        def test_assert_first_position_is_1()
+    	assert_equal(1, fizzbuzz().first)
+    end
+    def test_assert_fifteenth_position_is_mined_minds()
+		assert_equal("mined minds", fizzbuzz()[14])
+	end
 end
